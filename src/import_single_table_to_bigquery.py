84e071188a1c3571cb28c104c824ff8b53c11233
Ian <47256454+IanRFerguson@users.noreply.github.com>
2025-07-16 13:50:49 -0400

    can it be that easy?

--- src/import_single_table_to_bigquery.py
+++ src/import_single_table_to_bigquery.py
@@ -6,11 +6,16 @@ import psycopg2
 from utilities.config import BIGQUERY_DESTINATION_CONFIG, SQL_SOURCE_CONFIG
 from utilities.logger import logger
 from utilities.setup import get_jdbc_connection_string, set_dlt_environment_variables
+from dlt.sources.sql_database import sql_database
 
 ##########
 
 
 def pg_source():
+    """
+    Defines connection to remote Postgres and yields result
+    """
+
     with psycopg2.connect(
         host=os.environ["SOURCE_HOST"],
         port=os.environ["SOURCE_PORT"],
@@ -34,10 +39,10 @@ def pg_source():
 def run_import(
     vendor_name: str,
     source_schema_name: str,
-    source_table_name: str,
+    source_table_names: list,
     destination_schema_name: str,
-    destination_table_name: str,
     full_refresh: bool,
+    connection_string: str,
 ):
     """
     Executes an import from a remote host to the destination warehouse
@@ -52,13 +57,17 @@ def run_import(
         progress="log",
     )
 
-    logger.info(
-        f"Beginning pipeline tmc_{vendor_name} [{source_schema_name}.{source_table_name} -> {destination_schema_name}.{destination_table_name}]"
+    # Setup connection to source database
+    source_postgres_connection = sql_database(
+        credentials=connection_string,
+        schema=source_schema_name,
+        table_names=source_table_names,
+        chunk_size=10_000,
     )
 
+    # Kick off the read -> write
     info = pipeline.run(
-        pg_source(),
-        table_name=destination_table_name,
+        source_postgres_connection,
         write_disposition="replace",
     )
     logger.info(info)
@@ -79,7 +88,9 @@ if __name__ == "__main__":
     VENDOR_NAME = os.environ["VENDOR_NAME"]
 
     SOURCE_SCHEMA_NAME = os.environ["SOURCE_SCHEMA_NAME"]
-    SOURCE_TABLE_NAME = os.environ["SOURCE_TABLE_NAME"]
+    SOURCE_TABLE_NAMES = [
+        table.strip() for table in os.environ["SOURCE_TABLE_NAME"].split(",")
+    ]
     DESTINATION_SCHEMA_NAME = os.environ["DESTINATION_SCHEMA_NAME"]
     DESTINATON_TABLE_NAME = os.environ["DESTINATION_TABLE_NAME"]
 
@@ -88,8 +99,8 @@ if __name__ == "__main__":
     run_import(
         vendor_name=VENDOR_NAME.lower().replace(" ", "_"),
         source_schema_name=SOURCE_SCHEMA_NAME,
-        source_table_name=SOURCE_TABLE_NAME,
+        source_table_name=SOURCE_TABLE_NAMES,
         destination_schema_name=DESTINATION_SCHEMA_NAME,
-        destination_table_name=DESTINATON_TABLE_NAME,
         full_refresh=FULL_REFRESH,
+        connection_string=CONNECTION_STRING,
     )
