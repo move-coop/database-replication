373200d89bdd969d44f750b008ec627a4706e029
Skylar Pape <skylar.p@movementcooperative.org>
2025-11-05 18:44:21 -0600

    Adds WIP Postgres query
    TODO: Access Postgres secret from Google Secret Manager

--- src/pyspark/job.py
+++ src/pyspark/job.py
@@ -1,25 +1,55 @@
-### The following hello world job was copied & pasted from Google Search AI ###
-
+import click
 from pyspark.sql import SparkSession
 
-# Create a SparkSession
-# The .appName() method sets a name for your application, visible in the Spark UI.
-# .master("local[*]") configures Spark to run locally using all available cores.
-# .getOrCreate() retrieves an existing SparkSession or creates a new one if none exists.
-spark = SparkSession.builder \
-    .appName("HelloWorldPySpark") \
-    .master("local[*]") \
-    .getOrCreate()
+# Source DB Name => PySpark JDBC Driver
+SOURCE_DB_DRIVERS = {
+    "postgres": "org.postgresql.Driver",
+}
 
-# Perform a simple operation: print "Hello, Spark!"
-print("Hello, Spark!")
+@click.command()
+@click.option('--src-db-type', required=True, help='Type of source database (e.g. mysql, postgres)')
+@click.option('--src-protocol-action', default=None, help='Action specified in JDBC protocol for source database, if any')
+@click.option('--src-host', required=True, help='Host of source database')
+@click.option('--src-port', required=True, help='Port for source database, if any')
+@click.option('--src-db-name', required=True, help='Database name of source database')
+@click.option('--src-params', default=None, help='Connection parameters for source database, if any')
+@click.option('--src-user', required=True, help='User to connect to source database with')
+def main(
+    src_db_type: str,
+    src_protocol_action: str,
+    src_host: str,
+    src_port: int,
+    src_db_name: str,
+    src_params: str,
+    src_user: str,
+):
+    """
+    PySpark job that tests DB replication to GBQ via JDBC
+    """
+    src_jdbc_action = f":{src_protocol_action}" if src_protocol_action else ""
+    src_jdbc_params = f"?{src_params}" if src_params else ""
+    src_jdbc_url = f"jdbc:{src_db_type}{src_jdbc_action}://{src_host}:{src_port}/{src_db_name}{src_jdbc_params}"
 
-# Create a simple RDD (Resilient Distributed Dataset) and perform an action
-data = ["Hello", "World", "PySpark", "Job"]
-rdd = spark.sparkContext.parallelize(data)
+    if src_db_type not in SOURCE_DB_DRIVERS.keys():
+        raise Exception(f"Failed to find driver for unrecognized source database type: {src_db_type}")
+    src_driver = SOURCE_DB_DRIVERS[src_db_type]
+    src_properties = {
+        "user": src_user,
+        #"password": src_password,
+        "driver": src_driver
+    }
 
-# Count the number of elements in the RDD and print it
-print(f"Number of elements in RDD: {rdd.count()}")
+    spark = SparkSession.builder \
+        .appName("DbReplicationTest") \
+        .getOrCreate()
 
-# Stop the SparkSession
-spark.stop()
+    #query = "(SELECT * FROM public.message LIMIT 10) AS subquery"
+    #df = spark.read.jdbc(url=src_jdbc_url, table=query, properties=src_properties)
+    #df.show()
+
+    print(f"{src_user} @ {src_jdbc_url}")
+
+    spark.stop()
+
+if __name__ == '__main__':
+    main()
