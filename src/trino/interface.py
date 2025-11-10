1bdcbfb7743529d971cb3459a8b06dc8e003a6de
Ian <47256454+IanRFerguson@users.noreply.github.com>
2025-11-10 16:38:45 -0500

    simplify dramatically

--- src/trino/interface.py
+++ src/trino/interface.py
@@ -1,55 +1,48 @@
 import click
 from utilities.connection_clients import (
-    ConnectionClient,
-    SourceClient,
     TrinoClient,
 )
 from utilities.setup import (
-    SOURCE_DATABASE_CREDENTIALS,
     TRINO_CONNECTION_CREDENTIALS,
 )
 
 from common.logger import logger
+from typing import Optional
 
 #####
 
 
 @click.command()
 @click.option(
-    "--source-schema", required=True, help="Schema name in the source database"
-)
-@click.option("--source-table", required=True, help="Table name in the source database")
-@click.option(
-    "--row-chunk-size", default=10_000, help="Number of rows to transfer in each chunk"
+    "--source-catalog", required=True, type=str, help="Source Trino catalog name."
 )
 @click.option(
-    "--write-disposition",
-    default="truncate",
-    help="Write disposition for the data transfer",
+    "--source-schema", required=True, type=str, help="Source Trino schema name."
+)
+@click.option(
+    "--source-table", required=True, type=str, help="Source Trino table name."
+)
+@click.option(
+    "--destination-table",
+    required=False,
+    type=str,
+    help="Destination Trino table name.",
 )
 def main(
+    source_catalog: str,
     source_schema: str,
     source_table: str,
-    row_chunk_size: int,
-    write_disposition: str,
+    destination_table: Optional[str] = None,
 ):
     logger.debug("Setting up connectors...")
     trino_client = TrinoClient(**TRINO_CONNECTION_CREDENTIALS)
-    source_client = SourceClient(**SOURCE_DATABASE_CREDENTIALS)
 
-    logger.debug("Setting up connection client...")
-    connection_client = ConnectionClient(
-        source_client=source_client,
-        source_schema_name=source_schema,
-        source_table_name=source_table,
-        destination_client=trino_client,
-        row_chunk_size=row_chunk_size,
-        write_disposition=write_disposition,
+    trino_client.build_table(
+        source_catalog=source_catalog,
+        source_schema=source_schema,
+        source_table=source_table,
+        destination_table=destination_table,
     )
-
-    logger.info("Beginning to transfer data from source to Trino...")
-    logger.info(f"Source: {source_schema}.{source_table}")
-    connection_client.transfer()
 
 
 #####
